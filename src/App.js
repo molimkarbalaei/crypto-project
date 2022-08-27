59a9558c77b3c340d563ee967ad4b9dd4e312ae8
molim <mohade3.karbalaei@gmail.com>
2022-08-27 13:29:15 +0200

    create routes 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import react from "react";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  return <div>gfh</div>;
+  return (
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
@@ -9,3 +15,15 @@ export default App;
 // Language: javascript
 // first stage:
 // install tailwindcss
+// we use react router dom: so we need to install react router dom
+
+// **we have:
+// count page
+//coin page
+// home page
+// sign in page (login page)
+// sign up page (register page)
+
+// **nav bar:
+// it is in app.js
+// because we want to use it in all pages not only in home page
---
+++ src/routes/CoinPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CoinPage() {
+  return <div>CoinPage</div>;
+}
+
+export default CoinPage;
