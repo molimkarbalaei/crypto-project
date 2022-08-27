1afc0dcfebe0323fa0b3376b0da8403bdb645400
molim <mohade3.karbalaei@gmail.com>
2022-08-27 12:01:52 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "var(--color-bg-primary)",
+        secondary: "var(--color-bg-secondary)",
+        button: "var(--color-bg-button)",
+      },
+      textColor: {
+        accent: "var(--color-text-accent)",
+        primary: "var(--color-text-primary)",
+        secondary: "var(--color-text-secondary)",
+        btnText: "var(--color-bg-secondary)",
+      },
+      borderColor: {
+        primary: "var(--color-border-primary)",
+        secondary: "var(--color-border-secondary)",
+        input: "var(--color-bg-input)",
+        accent: "var(--color-text-accent)",
+      },
+    },
   },
   plugins: [],
 };
+
+// add darkmode to tailwind.config.js
+//add backgroundColor to tailwind.config.js
