b7d150d64390e0c8dd399db48d82d2e842bcd3c6
molim <mohade3.karbalaei@gmail.com>
2022-08-27 12:56:51 +0200

    feat: set up themes

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,52 @@
+// theme color (light or dark mode):
+
+import React, { createContext, useState, useEffect } from "react";
+// we want that if some one come to our page and change the theme color,
+//we want to update the theme color and remember that theme color:
+// so we need to create a context
+
+const getInitialTheme = () => {
+  // if the themecolor is undifiend:
+  if (typeof window !== "undefined" && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem("color-theme");
+    if (typeof storedPrefs === "string") {
+      return storedPrefs;
+    }
+    const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
+    if (userMedia.matches) {
+      return "dark";
+    }
+  }
+  return "light";
+};
+// 2- set up our context:
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ initialTheme, children }) => {
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  const rawSetTheme = (theme) => {
+    const root = window.document.documentElement;
+    const isDark = theme === "dark";
+
+    root.classList.remove(isDark ? "light" : "dark");
+    root.classList.add(theme);
+
+    localStorage.setItem("color-theme", theme);
+  };
+
+  if (initialTheme) {
+    rawSetTheme(initialTheme);
+  }
+
+  useEffect(() => {
+    rawSetTheme(theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
