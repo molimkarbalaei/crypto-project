25f377e8c4f9c12bb31eb988e7af233bcb01f75e
molim <mohade3.karbalaei@gmail.com>
2022-09-06 16:03:07 +0200

    styling table coins

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -6,23 +6,43 @@ import { Sparklines, SparklinesLine } from "react-sparklines";
 
 const CoinItem = ({ coin }) => {
   return (
-    <tr>
+    //tr = table row
+    <tr className="h-[80px] border-b overflow-hidden">
       {/* avali stare dovomi cap rank sevomi namade coin va esmesh  */}
       <td>
         <AiOutlineStar />
       </td>
       <td>{coin.market_cap_rank}</td>
       <td>
-        <div>
-          <img src={coin.image} alt={coin.id} />
-          <p>{coin.name}</p>
+        <div className="flex items-center">
+          <img
+            className="w-6 mr-2 rounded-full"
+            src={coin.image}
+            alt={coin.id}
+          />
+          <p className="hidden sm:table-cell">{coin.name}</p>
         </div>
       </td>
-      <td>{coin.symbol}</td>
-      <td>{coin.current_price}</td>
-      <td>{coin.price_change_percentage_24h}</td>
-      <td>{coin.total_volume}</td>
-      <td>{coin.market_cap}</td>
+      <td>{coin.symbol.toUpperCase()}</td>
+      <td>${coin.current_price.toLocaleString()}</td>
+      {/* red if is negetive green if is positive:*/}
+      <td>
+        {coin.price_change_percentage_24h > 0 ? (
+          <p className="text-green-600">
+            {coin.price_change_percentage_24h.toFixed(2)}%
+          </p>
+        ) : (
+          <p className="text-red-600">
+            {coin.price_change_percentage_24h.toFixed(2)}%
+          </p>
+        )}
+      </td>
+      <td className="w-[180px] hidden md:table-cell">
+        ${coin.total_volume.toLocaleString()}
+      </td>
+      <td className="w-[180px] hidden sm:table-cell">
+        ${coin.market_cap.toLocaleString()}
+      </td>
       <td>
         <Sparklines data={coin.sparkline_in_7d.price}>
           <SparklinesLine color="teal" />
