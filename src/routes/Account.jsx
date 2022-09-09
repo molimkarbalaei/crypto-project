42f64aea93a3856971f3d7bccdb7bb45f89001d3
molim <mohade3.karbalaei@gmail.com>
2022-09-09 20:36:16 +0200

    add protected routes

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import SavedCoin from "../components/SavedCoin";
 // we wanna create protected route :
 import { UserAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Account() {
   const { user, logout } = UserAuth();
@@ -20,36 +20,41 @@ function Account() {
     }
   };
 
-  return (
-    <div className="max-w-[1140px] mx-auto">
-      <div className="flex justify-between items-bcenter my-12 py-8 rounded-div">
-        <div>
-          <h1 className="text-2xl font-bold">Account</h1>
+  /// we wanna create protected route :
+  if (user) {
+    return (
+      <div className="max-w-[1140px] mx-auto">
+        <div className="flex justify-between items-bcenter my-12 py-8 rounded-div">
           <div>
-            {/* the user would be dynamic */}
-            <p>Welcome {user?.email}</p>
+            <h1 className="text-2xl font-bold">Account</h1>
+            <div>
+              {/* the user would be dynamic */}
+              <p>Welcome {user?.email}</p>
+            </div>
+          </div>
+
+          <div>
+            <button
+              onClick={handleSignOut}
+              className="bg-blue-100 border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
+            >
+              Sign Out
+            </button>
           </div>
         </div>
 
-        <div>
-          <button
-            onClick={handleSignOut}
-            className="bg-blue-100 border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl"
-          >
-            Sign Out
-          </button>
+        <div className="flex justify-between items-center my-12 py-8 rounded-div">
+          <div className="w-full min-h-[300px]">
+            {/* we create a new component for saved coins */}
+            <h1 className="font-bold text-2xl py-4">Saved Coins </h1>
+            <SavedCoin />
+          </div>
         </div>
       </div>
-
-      <div className="flex justify-between items-center my-12 py-8 rounded-div">
-        <div className="w-full min-h-[300px]">
-          {/* we create a new component for saved coins */}
-          <h1 className="font-bold text-2xl py-4">Saved Coins </h1>
-          <SavedCoin />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <Navigate to="/signin" />;
+  }
 }
 
 export default Account;
